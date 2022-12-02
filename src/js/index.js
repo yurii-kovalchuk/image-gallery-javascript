5134aae745c8f14f64d69d0f5ac85308540124d7
yurii-kovalchuk <sogdrums@gmail.com>
2022-12-02 23:18:46 +0200

    testing-2

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,12 @@ import axios from 'axios';
 async function createRequest(value, page) {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api?key=31776776-892f87ec0bcca7b792e7dfca0&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+      `https://pixabay.com/api?key=31776776-892f87ec0bcca7b792e7dfca0&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
     );
     return response.data;
   } catch (error) {
